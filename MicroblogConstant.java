7c9bac2a544b315e4e3638289ede1beb446f1e86
jobyang <905373499@qq.com>
2017-09-08 14:45:14 +0800

    my test

---
+++ MicroblogConstant.java
@@ -0,0 +1,32 @@
+/**   
+* Filename:    MicroblogConstant.java   
+* @version:    1.0  
+* Create at:   2014年10月15日 上午3:27:50   
+* Description:  
+*   
+* Modification History:   
+* Date        Author      Version     Description   
+* ----------------------------------------------------------------- 
+* 2014年10月15日    shiyl      1.0         1.0 Version   
+*/
+package com.cnfantasia.server.api.microblog.constant;
+
+import java.math.BigInteger;
+
+/**
+ * Filename:    MicroblogConstant.java
+ * @version:    1.0.0
+ * Create at:   2014年10月15日 上午3:27:50
+ * Description:
+ *
+ * Modification History:
+ * Date           Author           Version           Description
+ * ------------------------------------------------------------------
+ * 2014年10月15日       shiyl             1.0             1.0 Version
+ */
+public class MicroblogConstant {
+	/**
+	 * 发送微博内容为空时默认使用类别Id
+	 */
+	public static final BigInteger DEFAULT_MICROBLOG_TYPEID = new BigInteger("3");
+}
